56707db013156740c8e78b5618775b7fb194d848
Tung <tung09122000@gmail.com>
2021-03-09 16:28:32 +0700

    Connet Database

--- SmartTool-API/Startup.cs
+++ SmartTool-API/Startup.cs
@@ -6,32 +6,40 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using SmartTool_API.Data;
 
 namespace SmartTool_API
 {
     public class Startup
     {
+        private string factoryConnection;
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
+            factoryConnection = configuration.GetSection("AppSettings:Factory").Value + "Connection";
         }
 
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        // public void ConfigureServices(IServiceCollection services)
-        // {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors();
+            services.AddDbContext<DataContext>(options => options.UseSqlServer(Configuration.GetConnectionString(factoryConnection)));
+            services.AddDbContext<SHCDataContext>(options => options.UseSqlServer(Configuration.GetConnectionString("SHCConnection")));
 
-        //     services.AddControllers();
-        //     services.AddSwaggerGen(c =>
-        //     {
-        //         c.SwaggerDoc("v1", new OpenApiInfo { Title = "SmartTool_API", Version = "v1" });
-        //     });
-        // }
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "SmartTool_API", Version = "v1" });
+            });
+        }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
