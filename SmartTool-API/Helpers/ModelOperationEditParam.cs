5c60093c153b0c06a98c926c4f97cfc93cdb504e
Tung <tung09122000@gmail.com>
2021-03-09 16:31:14 +0700

    Create Helper ModelOperationEditParam

---
+++ SmartTool-API/Helpers/ModelOperationEditParam.cs
@@ -0,0 +1,10 @@
+namespace SmartTool_API.Helpers
+{
+    public class ModelOperationEditParam
+    {
+        public string factory_id { get; set; }
+        public string model_no { get; set; }
+        public string stage_id { get; set; }
+        public string operation_id { get; set; }
+    }
+}
