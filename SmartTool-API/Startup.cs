88e99fb4f010f3f177d2f5cfa1f6c5d8bdb2041f
Tung <tung09122000@gmail.com>
2021-03-10 15:27:54 +0700

    Thêm repository, Service,... vào Startup

--- SmartTool-API/Startup.cs
+++ SmartTool-API/Startup.cs
@@ -1,7 +1,10 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -11,8 +14,14 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
+using SmartTool_API._Repositories.Interfaces;
+using SmartTool_API._Repositories.Repositories;
+using SmartTool_API._Services.Interfaces;
+using SmartTool_API._Services.Services;
 using SmartTool_API.Data;
+using SmartTool_API.Helpers.AutoMapper;
 
 namespace SmartTool_API
 {
@@ -33,14 +42,74 @@ namespace SmartTool_API
             services.AddCors();
             services.AddDbContext<DataContext>(options => options.UseSqlServer(Configuration.GetConnectionString(factoryConnection)));
             services.AddDbContext<SHCDataContext>(options => options.UseSqlServer(Configuration.GetConnectionString("SHCConnection")));
+            services.AddControllers().AddNewtonsoftJson();
 
-            
+            //Auto Mapper
+            services.AddAutoMapper(typeof(Startup));
+            services.AddScoped<IMapper>(sp =>
+            {
+                return new Mapper(AutoMapperConfig.RegisterMappings());
+            });
+            services.AddSingleton(AutoMapperConfig.RegisterMappings());
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.RequireHttpsMetadata = false;
+                    options.SaveToken = true;
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII
+                        .GetBytes(Configuration.GetSection("AppSettings:Token").Value)),
+                        ValidateIssuer = false,
+                        ValidateAudience = false
+                    };
+                });
 
-            services.AddControllers();
+            // Repository
+            services.AddScoped<IDefectReasonRepository, DefectReasonRepository>();
+            services.AddScoped<IKaizenRepository, KaizenRepository>();
+            services.AddScoped<IMeasurement_RFTRepository, Measurement_RFTRepository>();
+            services.AddScoped<IModelOperationRepository, ModelOperationRepository>();
+            services.AddScoped<IModelRepository, ModelRepository>();
+            services.AddScoped<IModelTypeRepository, ModelTypeRepository>();
+            services.AddScoped<IProcessTypeRepository, ProcessTypeRepository>();
+            services.AddScoped<IStageRepository, StageRepository>();
+
+            // Service
+            // services.AddScoped<IDefectReasonService, DefectReasonService>();
+            services.AddScoped<IKaizenService, KaizenService>();
+            services.AddScoped<IModelService, ModelService>();
+            services.AddScoped<IModelOperationService, ModelOperationService>();
+            // services.AddScoped<IRFTReportService, RFTReportService>();
+
+            // Swagger
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "SmartTool_API", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "SmartTooling API", Version = "v1" });
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Description = "Please insert JWT with Bearer into field",
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey
+                });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        }
+                        },
+                        new string[] { }
+                        }
+                    });
             });
+
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -49,20 +118,27 @@ namespace SmartTool_API
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                // app.UseSwagger();
-                // app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "SmartTool_API v1"));
             }
-
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
             app.UseHttpsRedirection();
-
             app.UseRouting();
+            // app.UseCors ("CorsPolicy");
+            app.UseStaticFiles();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
             });
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "SmartTool API V1");
+            });
+
         }
     }
 }
