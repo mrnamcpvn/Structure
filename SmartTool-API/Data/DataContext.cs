ae34cceedccc99e58993e0e954c55ffe268bbb1e
Tung <tung09122000@gmail.com>
2021-03-10 10:03:16 +0700

    Code thêm Model và DTO

--- SmartTool-API/Data/DataContext.cs
+++ SmartTool-API/Data/DataContext.cs
@@ -8,6 +8,9 @@ namespace SmartTool_API.Data
     {
         public DbSet<Model> Models { get; set; }
         public DbSet<Model_Operation> Model_Operations { get; set; }
+        public DbSet<Kaizen> Kaizen { get; set; }
+        public DbSet<Measurement_RFT> Measurement_RFT { get; set; }
+
 
         protected override void OnModelCreating(ModelBuilder modelBuilder){
             modelBuilder.Entity<Model>().HasKey(x => new { x.factory_id, x.model_no });
---
+++ SmartTool-API/Models/Stage.cs
@@ -0,0 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace SmartTool_API.Models
+{
+    public partial class Stage
+    {
+        [Key]
+        [StringLength(50)]
+        public string factory_id { get; set; }
+        [Key]
+        [StringLength(8)]
+        public string stage_id { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string stage_name { get; set; }
+        public int sequence { get; set; }
+        public bool is_active { get; set; }
+    }
+}
