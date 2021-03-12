a14ff6062ad1cce8080e54e52b04ca49a67e5e52
Tung <tung09122000@gmail.com>
2021-03-12 16:07:31 +0700

    Delete những _Repository, _Services dư

--- SmartTool-API/_Repositories/Interfaces/IProcessTypeRepository.cs
+++
@@ -1,9 +0,0 @@
-using SmartTool_API.Models;
-
-namespace SmartTool_API._Repositories.Interfaces
-{
-    public interface IProcessTypeRepository: IMainRepository<Process_Type>
-    {
-         
-    }
-}
