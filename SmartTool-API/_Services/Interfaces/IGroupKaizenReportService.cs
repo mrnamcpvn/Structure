6c0783b8bd42634a969dcdcf3d918ced421e27f5
Dang Khoa <dangkhoa080998@gmail.com>
2021-04-26 16:08:18 +0700

    Update Group KaizenReport

--- SmartTool-API/_Services/Interfaces/IGroupKaizenReportService.cs
+++ SmartTool-API/_Services/Interfaces/IGroupKaizenReportService.cs
@@ -9,14 +9,12 @@ namespace SmartTool_API._Services.Interfaces
     public interface IGroupKaizenReportService
     {
         Task<List<Factory>> GetAllFactory();
-        Task<PagedList<Model>> Search(PaginationParam param, KaizenReportGroupParam filterParam);
+        Task<PagedList<Model>> Search(PaginationParam param,KaizenReportGroupParam filterParam);
         Task<List<VW_ModelKaizen_Dto>> GetModelKaizens(KaizenReportGroupParam filterParam);
         Task<Model> GetModelByModelNo(string factory_id, string model_No);
         Task<List<string>> GetSeasonByUpper(string factory_id, string upper_id);
 
-
-        //load data chart
-
+        // Load data Chart
         Task<List<Efficiency>> GetEfficiencys(string factory_id, string upper_id, string season);
         Task<PagedList<KaizenModelDetail>> GetKaiZens(PaginationParam param, string factory_id, string model_no);
         Task<object> GetKaizenDetail(string factory_id, string model_no, string serial_no);
